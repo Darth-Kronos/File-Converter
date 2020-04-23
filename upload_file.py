3945b9d49a26e163d106e90db158f9afd7f7c851
Darth-Kronos <purushothaman.syadav@gmail.com>
2020-04-23 18:01:43 +0530

    Upload

--- upload_file.py
+++ upload_file.py
@@ -8,7 +8,6 @@ UPLOAD_FOLDER = "/home/puru/Documents/College/6_sem/CN/"
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'vnkdjnfjknfl1232#'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 
 def allowed_file(filename):
@@ -20,12 +19,8 @@ def convert(filename):
     file = os.path.splitext(filename)[0] + ".png"
     im1.save(os.path.join(app.config['UPLOAD_FOLDER'], file))
 
-@app.route("/")
-@app.route("/home")
-def home():
-    return render_template('home.html')
-@app.route("/image",methods=['GET', 'POST'])
-def image():
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
@@ -44,16 +39,19 @@ def image():
             file = os.path.splitext(filename)[0] + ".png"
             return redirect(url_for('uploaded_file',
                                     filename=file))
-    return render_template('image.html')
-@app.route('/success')
-def success():
-    return render_template('success.html')
-@app.route('/download')
-def download_file():
-	#path = "html2pdf.pdf"
-	#path = "info.xlsx"
-	path = "simple.docx"
-	#path = "sample.txt"
-	return send_file(path, as_attachment=True)
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    '''
+@app.route("/uploaded_file")
+def uploaded_file():
+    filename = request.args.get('filename',None)
+    return send_file(os.path.join(app.config['UPLOAD_FOLDER'],
+                                filename),as_attachment=True)
 if __name__ == '__main__':
     app.run(debug=True)
